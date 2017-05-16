f4cf0e7971551b4cec5b3e0bace8c3df16d2a467
SAURABHIMA <saurabhima@gmail.com>
2017-05-17 03:04:28 +0530

    Added new fields to the pickle file including title,contact person and contact date.

--- gsoc_LEADERBOARD.py
+++ gsoc_LEADERBOARD.py
@@ -23,10 +23,14 @@ def add_donors():
 
 @app.route('/process_donor_form',methods=['POST'])
 def process_add_donors():
+    title=request.form['donor_title']
     name = request.form['donor_name']
     org = request.form['donor_org']
+    email=request.form['donor_email']
     donated_amount = request.form['donated_amount']
     donor_contact=request.form['donor_contact']
+    contact_person=request.form['contact_person']
+    contact_date=request.form['contact_date']
     donor_list=[]
     donor_pickle_filename = 'donor_details.pickle'
 
@@ -38,10 +42,14 @@ def process_add_donors():
 
 
     donor={}
+    donor['title'] = title
     donor['name']=name
     donor['org']=org
     donor['amount']=donated_amount
     donor['phone']=donor_contact
+    donor['email'] = email
+    donor['contact_person'] = contact_person
+    donor['contact_date'] = contact_date
     donor['donation_date']=datetime.datetime.now().date().strftime ("%Y-%m-%d")
     donor_list.append(donor)
 
