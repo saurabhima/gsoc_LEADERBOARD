d2cf61bcb6cf7a0f59388152ed41df53233c80e3
sierra <saurabhima@gmail.com>
2017-06-14 15:58:56 +0530

    Added Phone Logging to Donor Workflow.

--- leaderboard.py
+++ leaderboard.py
@@ -11,6 +11,7 @@ app.secret_key = 'abc1234'
 app.config['DEBUG'] = True
 
 
+# Default Index and testing Pages
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -21,6 +22,7 @@ def default():
     return render_template('default_testing.html')
 
 
+#  Donor Leader Board Page
 @app.route('/donorleaderboard')
 def donor_leaderboard():
     donor_details_file = config.DONOR_DETAILS_PICKLE_FILE
@@ -32,6 +34,9 @@ def donor_leaderboard():
     return render_template('donor_leaderboard.html', donor_list=pickle_obj)
 
 
+########## User Management Sub Modules #########
+
+# New User Registration
 @app.route('/user_register')
 def user_register():
     return render_template('register_new_user.html')
@@ -64,7 +69,7 @@ def process_new_user_register():
     user['username'] = username
     user['password'] = password
     user['user_type'] = user_type
-    user['register_date'] = datetime.datetime.now().date().strftime("%Y-%m-%d")
+    user['register_date'] = datetime.datetime.now().date().strftime("%m-%d-%Y")
     user['account_status'] = 'Pending'
     user_list.append(user)
 
@@ -78,6 +83,7 @@ def process_new_user_register():
     return redirect(url_for('index'))
 
 
+# User Login and Verification
 @app.route('/user_login')
 def user_login():
     return render_template('user_login.html')
@@ -102,48 +108,7 @@ def user_login_process():
         return redirect(url_for('index'))
 
 
-@app.route('/register_new_donor')
-def register_new_donor():
-    if session['login_status'] == 'True' and (
-            session['logged_user_type'] == 'Administrator' or session['logged_user_type'] == 'Supervisor'):
-        return render_template('register_new_donor.html')
-    else:
-        return render_template('invalid_login.html')
-
-
-@app.route('/process_donor_form', methods=['POST'])
-def process_add_donors():
-    title = request.form['donor_title']
-    name = request.form['donor_name']
-    org = request.form['donor_org']
-    email = request.form['donor_email']
-
-    donor_contact = request.form['donor_contact']
-    contact_person = request.form['contact_person']
-    contact_date = request.form['contact_date']
-    donor_list = []
-    donor_pickle_filename = 'donor_details.pickle'
-
-    if os.path.exists(donor_pickle_filename):
-        with open(donor_pickle_filename, 'rb') as rfp:
-            donor_list = pickle.load(rfp)
-    donor = {}
-    donor['title'] = title
-    donor['name'] = name
-    donor['org'] = org
-
-    donor['phone'] = donor_contact
-    donor['email'] = email
-    donor['contact_person'] = contact_person
-    donor['contact_date'] = contact_date
-    donor_list.append(donor)
-
-    with open(donor_pickle_filename, 'wb') as wfp:
-        pickle.dump(donor_list, wfp)
-
-    return redirect(url_for('index'))
-
-
+# User Logout
 @app.route('/logout')
 def logout():
     session.pop('logged_username', None)
@@ -154,5 +119,121 @@ def logout():
     return redirect(url_for('index'))
 
 
+# Approve New User
+@app.route('/approve_new_user')
+def approve_new_user():
+    user_list = sub_process.get_pending_user_list()
+    if len(user_list) > 0:
+        return render_template('approve_user.html', user=user_list[0])
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/approve_new_user_process', methods=['POST'])
+def approve_new_user_process():
+    print 'user approval process module'
+    username = request.form['username']
+    if request.form['submit'] == 'Approve':
+        sub_process.approve_user(username)
+        print 'User Approved'
+        return redirect(url_for('approve_new_user'))
+    else:
+        sub_process.reject_user(username)
+        print 'User Rejected'
+        return redirect(url_for('approve_new_user'))
+
+
+########## Donor Management Sub Modules #########
+
+# Register New Donor to Database
+@app.route('/register_new_donor')
+def register_new_donor():
+    try:
+        if session['login_status'] == 'True' and (
+            session['logged_user_type'] == 'Administrator' or session['logged_user_type'] == 'Supervisor'):
+            return render_template('register_new_donor.html')
+        else:
+            return render_template('invalid_login.html')
+    except:
+
+        return render_template('invalid_login.html')
+@app.route('/process_donor_form', methods=['POST'])
+def process_add_donors():
+    title = request.form['donor_title']
+    name = request.form['donor_name']
+    org = request.form['donor_org']
+    email = request.form['donor_email']
+    donor_contact = request.form['donor_contact']
+    contact_person = request.form['contact_person']
+    contact_date = request.form['contact_date']
+    anonymous_select = request.form['anonymous_select']
+    donor_list = []
+    donor_pickle_filename = 'donor_details.pickle'
+
+    donor_list_len = 0
+    if os.path.exists(donor_pickle_filename):
+        with open(donor_pickle_filename, 'rb') as rfp:
+            donor_list = pickle.load(rfp)
+            donor_list_len = len(donor_list)
+
+    donor = {}
+    donor['id'] = donor_list_len + 1
+    donor['title'] = title
+    donor['name'] = name
+    donor['org'] = org
+    donor['phone'] = donor_contact
+    donor['email'] = email
+    donor['contact_person'] = contact_person
+    donor['contact_date'] = contact_date
+    donor['anonymous_select'] = anonymous_select
+    donor['donor_status'] = 'New'
+    donor_list.append(donor)
+
+    with open(donor_pickle_filename, 'wb') as wfp:
+        pickle.dump(donor_list, wfp)
+
+    return redirect(url_for('index'))
+
+
+# Donor Phone Contact
+@app.route('/donor_phone_contact')
+def donor_phone_contact():
+    donor_details_file = config.DONOR_DETAILS_PICKLE_FILE
+    pickle_obj = None
+    if os.path.exists(donor_details_file):
+        fh = open(donor_details_file, 'rb')
+        pickle_obj = pickle.load(fh)
+    return render_template('donor_phone_contact.html', donor_list=pickle_obj)
+
+
+@app.route('/donor_phone_contact_process', methods=['POST'])
+def donor_phone_contact_process():
+    donor_id = request.form['submit']
+    donor_obj = sub_process.donor_details_byid(donor_id)
+    donor_previous_logs = sub_process.donor_contact_logs_byid(donor_id)
+    current_date = datetime.datetime.now().date().strftime("%m-%d-%Y")
+    current_time = datetime.datetime.now().time().strftime("%H:%M")
+    return render_template('donor_phone_contact_logging.html', donor_obj=donor_obj, contact_log=donor_previous_logs,
+                           current_date=current_date, current_time=current_time)
+
+
+@app.route('/donor_phone_log_process', methods=['POST'])
+def donor_phone_log_process():
+    contact_person = request.form['contact_person']
+    donor_id = request.form['donor_id']
+    contact_date = request.form['contact_date']
+    contact_time = request.form['contact_time']
+    details_shared = request.form['details_shared']
+    remarks = request.form['remarks']
+    donor_log = {}
+    donor_log['donor_id'] = donor_id
+    donor_log['contact_person'] = contact_person
+    donor_log['contact_date'] = contact_date
+    donor_log['contact_time'] = contact_time
+    donor_log['contact_mode'] = 'Phone'
+    donor_log['details_shared'] = details_shared
+    donor_log['remarks'] = remarks
+    sub_process.add_donor_log(donor_log)
+    return redirect(url_for('donor_phone_contact'))
 if __name__ == '__main__':
     app.run()
--- sub_process.py
+++ sub_process.py
@@ -34,3 +34,103 @@ def write_user_log(str):
     userlogfh.write(str)
     userlogfh.write('\n')
     userlogfh.close()
+
+
+def get_donor_list():
+    donor_details_file = config.DONOR_DETAILS_PICKLE_FILE
+    pickle_obj = None
+    if os.path.exists(donor_details_file):
+        fh = open(donor_details_file, 'rb')
+        pickle_obj = pickle.load(fh)
+        print(pickle_obj)
+    return pickle_obj
+
+
+def get_pending_user_list():
+    user_detail_file = config.USER_DETAILS_PICKLE_FILE
+    pending_user_list = []
+    if os.path.exists(user_detail_file):
+        fh = open(user_detail_file, 'rb')
+        pickle_obj = pickle.load(fh)
+        obj_len = len(pickle_obj)
+        for i in range(0, obj_len):
+            if pickle_obj[i]['account_status'] == 'Pending':
+                pending_user_list.append(pickle_obj[i])
+
+    return pending_user_list
+
+
+def approve_user(username):
+    user_detail_file = config.USER_DETAILS_PICKLE_FILE
+    user_list = []
+    if os.path.exists(user_detail_file):
+        fh = open(user_detail_file, 'rb')
+        pickle_obj = pickle.load(fh)
+        obj_len = len(pickle_obj)
+        for i in range(0, obj_len):
+            if pickle_obj[i]['username'] != username:
+                user_list.append(pickle_obj[i])
+            elif pickle_obj[i]['username'] == username:
+                pickle_obj[i]['account_status'] = 'Active'
+                user_list.append(pickle_obj[i])
+        fh.close()
+        with open(user_detail_file, 'wb') as wfp:
+            pickle.dump(user_list, wfp)
+
+
+def reject_user(username):
+    user_detail_file = config.USER_DETAILS_PICKLE_FILE
+    user_list = []
+    if os.path.exists(user_detail_file):
+        fh = open(user_detail_file, 'rb')
+        pickle_obj = pickle.load(fh)
+        obj_len = len(pickle_obj)
+        for i in range(0, obj_len):
+            if pickle_obj[i]['username'] != username:
+                user_list.append(pickle_obj[i])
+            fh.close()
+        with open(user_detail_file, 'wb') as wfp:
+            pickle.dump(user_list, wfp)
+
+
+def donor_details_byid(donor_id):
+    donor_details_file = config.DONOR_DETAILS_PICKLE_FILE
+    pickle_obj = None
+    if os.path.exists(donor_details_file):
+        fh = open(donor_details_file, 'rb')
+        pickle_obj = pickle.load(fh)
+        obj_len = len(pickle_obj)
+        for i in range(0, obj_len):
+            if int(pickle_obj[i]['id']) == int(donor_id):
+                return pickle_obj[i]
+    return None
+
+
+def donor_contact_logs_byid(donor_id):
+    donor_contact_log_file = config.DONOR_CONTACT_LOGS_FILE
+    pickle_obj = None
+    contact_logs = []
+    if os.path.exists(donor_contact_log_file):
+        fh = open(donor_contact_log_file, 'rb')
+        pickle_obj = pickle.load(fh)
+        obj_len = len(pickle_obj)
+        for i in range(0, obj_len):
+            if int(pickle_obj[i]['donor_id']) == int(donor_id):
+                contact_logs.append(pickle_obj[i])
+    return contact_logs
+
+
+def add_donor_log(donor_log):
+    donor_contact_log_file = config.DONOR_CONTACT_LOGS_FILE
+    pickle_obj = None
+    contact_logs = []
+    if os.path.exists(donor_contact_log_file):
+        fh = open(donor_contact_log_file, 'rb')
+        pickle_obj = pickle.load(fh)
+        obj_len = len(pickle_obj)
+        fh.close()
+        for i in range(0, obj_len):
+            contact_logs.append(pickle_obj[i])
+    contact_logs.append(donor_log)
+    with open(donor_contact_log_file, 'wb') as wfp:
+        pickle.dump(contact_logs, wfp)
