e55c48a4a383b042d4573542ea0292d194956ef0
Saurabh Singh <saurabhima@gmail.com>
2017-07-02 22:33:54 +0530

    Added Schema Diagram

--- leaderboard.py
+++ leaderboard.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = 'abc1234'
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dbadmin:dbadministrator@localhost/donorworkflow'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dbadmin:dbadministrator@localhost/donorworkflow'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://SAURABHIMA:dbadministrator@SAURABHIMA.mysql.' \
+                                        'pythonanywhere-services.com/donorworkflow'
 db = SQLAlchemy(app)
 
 from views import *
